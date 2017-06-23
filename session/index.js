e1ca601c7d30ff270b440d958e04c8187cf8fee7
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2017-06-24 03:15:37 +1000

    Created a session class

---
+++ session/index.js
@@ -0,0 +1,41 @@
+'use strict';
+const {findById} = require('../utils');
+const crypto = require('crypto');
+
+const sessionStore = new Map();
+const session = {
+    init(id) {
+        // find a session object by facebook id
+        let sessionId = findById(id, sessionStore);
+        if (sessionId) {
+            return sessionId;
+        }
+        else {
+            // instantiate a new sessionId
+            // sessionId, context object
+            let newSessionID = crypto.randomBytes(12).toString('hex');
+            let obj = {
+                fbid: id,
+                context: {}
+            };
+            sessionStore.set(newSessionID, obj);
+            return newSessionID;
+        }
+    },
+
+    get(sessionId) {
+        return sessionStore.get(sessionId);
+    },
+
+    update(sessionId, context) {
+        let obj = sessionStore.get(sessionId);
+        obj.context = context;
+        return sessionStore.set(sessionId, obj);
+    },
+
+    delete(sessionId) {
+        return sessionStore.delete(sessionId);
+    }
+};
+
+module.exports = session;
