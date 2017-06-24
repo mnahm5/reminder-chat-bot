0c4fb338cd3727b5f358a2a56b38f200a97b2e9f
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2017-06-25 09:26:48 +1000

    sending back reminders

--- agenda/index.js
+++ agenda/index.js
@@ -10,6 +10,12 @@ const createReminder = require('./createReminder');
 module.exports = (f) => {
     // define agenda jobs
 
+    // display a reminder
+    agenda.define('reminder', job => {
+        const {fbid, first_name, task} = job.attrs.data;
+        f.txt(fbid, `Hey ${first_name}! Reminding you to ${task}`)
+    });
+
     //create reminder
     createReminder(agenda, f);
 
