b6ef8ea77295e1d164b67482000ab86dc5eff076
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2017-06-24 11:00:44 +1000

    working with timezones

--- agenda/createReminder.js
+++ agenda/createReminder.js
@@ -5,5 +5,13 @@ module.exports = (agenda, f) => {
         // extract fbid, datetime and take
         const {fbid, datetime, task} = job.attrs.data;
 
+        // get the fb user's timezone
+        f.getProfile(fbid)
+            .then(profile => {
+                const {first_name, timezone} = profile;
+            })
+            .catch(error => console.log(error))
+
+        // compute the offset
     });
 };
--- fbeamer/index.js
+++ fbeamer/index.js
@@ -3,157 +3,176 @@ const request = require('request');
 const crypto = require('crypto');
 
 class FBeamer {
-	constructor(config) {
-		try {
-			if(!config || config.PAGE_ACCESS_TOKEN === undefined || config.VERIFY_TOKEN === undefined || config.APP_SECRET === undefined) {
-				throw new Error("Unable to access tokens!");
-			} else {
-				this.PAGE_ACCESS_TOKEN = config.PAGE_ACCESS_TOKEN;
-				this.VERIFY_TOKEN = config.VERIFY_TOKEN;
-				this.APP_SECRET = config.APP_SECRET;
-			}
-		} catch(e) {
-			console.log(e);
-		}
-	}
+    constructor(config) {
+        try {
+            if(!config || config.PAGE_ACCESS_TOKEN === undefined || config.VERIFY_TOKEN === undefined || config.APP_SECRET === undefined) {
+                throw new Error("Unable to access tokens!");
+            } else {
+                this.PAGE_ACCESS_TOKEN = config.PAGE_ACCESS_TOKEN;
+                this.VERIFY_TOKEN = config.VERIFY_TOKEN;
+                this.APP_SECRET = config.APP_SECRET;
+            }
+        } catch(e) {
+            console.log(e);
+        }
+    }
 
-	registerHook(req, res) {
-		// If req.query.hub.mode is 'subscribe'
-		// and if req.query.hub.verify_token is the same as this.VERIFY_TOKEN
-		// then send back an HTTP status 200 and req.query.hub.challenge
-		let {
-			mode, 
-			verify_token, 
-			challenge
-		} = req.query.hub;
+    registerHook(req, res) {
+        // If req.query.hub.mode is 'subscribe'
+        // and if req.query.hub.verify_token is the same as this.VERIFY_TOKEN
+        // then send back an HTTP status 200 and req.query.hub.challenge
+        let {
+            mode,
+            verify_token,
+            challenge
+        } = req.query.hub;
 
-		if(mode === 'subscribe' && verify_token === this.VERIFY_TOKEN) {
-			return res.end(challenge);
-		} else {
-			console.log("Could not register webhook!");
-			return res.status(403).end();
-		}
-	}
+        if(mode === 'subscribe' && verify_token === this.VERIFY_TOKEN) {
+            return res.end(challenge);
+        } else {
+            console.log("Could not register webhook!");
+            return res.status(403).end();
+        }
+    }
 
-	verifySignature(req, res, next) {
-		if(req.method === 'POST') {
-			let signature = req.headers['x-hub-signature'];
-			try {
-				if(!signature) {
-					throw new Error("Signature missing!");
-				} else {
-					let hash = crypto.createHmac('sha1', this.APP_SECRET).update(JSON.stringify(req.body)).digest('hex');
-					try {
-						if(hash !== signature.split("=")[1]) {
-							throw new Error("Invalid Signature");
-						} 
-					} catch(e) {
-							res.send(500, e);
-						}
-				}
-			} catch(e) {
-				res.send(500, e);
-			}
-		} 
+    verifySignature(req, res, next) {
+        if(req.method === 'POST') {
+            let signature = req.headers['x-hub-signature'];
+            try {
+                if(!signature) {
+                    throw new Error("Signature missing!");
+                } else {
+                    let hash = crypto.createHmac('sha1', this.APP_SECRET).update(JSON.stringify(req.body)).digest('hex');
+                    try {
+                        if(hash !== signature.split("=")[1]) {
+                            throw new Error("Invalid Signature");
+                        }
+                    } catch(e) {
+                        res.send(500, e);
+                    }
+                }
+            } catch(e) {
+                res.send(500, e);
+            }
+        }
 
-		return next();
+        return next();
 
-	}
+    }
 
-	subscribe() {
-		request({
-			uri: 'https://graph.facebook.com/v2.6/me/subscribed_apps',
-			qs: {
-				access_token: this.PAGE_ACCESS_TOKEN
-			},
-			method: 'POST'
-		}, (error, response, body) => {
-			if(!error && JSON.parse(body).success) {
-				console.log("Subscribed to the page!");
-			} else {
-				console.log(error);
-			}
-		});
-	}
+    subscribe() {
+        request({
+            uri: 'https://graph.facebook.com/v2.6/me/subscribed_apps',
+            qs: {
+                access_token: this.PAGE_ACCESS_TOKEN
+            },
+            method: 'POST'
+        }, (error, response, body) => {
+            if(!error && JSON.parse(body).success) {
+                console.log("Subscribed to the page!");
+            } else {
+                console.log(error);
+            }
+        });
+    }
 
-	incoming(req, res, cb) {
-		// Extract the body of the POST request
-		let data = req.body;
-		if(data.object === 'page') {
-			// Iterate through the page entry Array
-			data.entry.forEach(pageObj => {
-				// Iterate through the messaging Array
-				pageObj.messaging.forEach(msgEvent => {
-					let messageObj = {
-						sender: msgEvent.sender.id,
-						timeOfMessage: msgEvent.timestamp,
-						message: msgEvent.message || undefined,
-						postback: msgEvent.postback || undefined 
-					}
-					
-					cb(messageObj);
-				});
-			});
-		}
-		res.send(200);
-	}
+    getProfile(id) {
+        return new Promise((resolve, reject) => {
+            request({
+                uri: `https://graph.facebook.com/v2.7/${id}`,
+                qs: {
+                    access_token: this.PAGE_ACCESS_TOKEN
+                },
+                method: 'GET'
+            }, (error, response, body) => {
+                if (!error && response.statusCode === 200) {
+                    resolve(JSON.parse(body));
+                }
+                else {
+                    reject(error);
+                }
+            });
+        });
+    }
 
-	sendMessage(payload) {
-		return new Promise((resolve, reject) => {
-			// Create an HTTP POST request
-			request({
-				uri: 'https://graph.facebook.com/v2.6/me/messages',
-				qs: {
-					access_token: this.PAGE_ACCESS_TOKEN
-				},
-				method: 'POST',
-				json: payload
-			}, (error, response, body) => {
-				if(!error && response.statusCode === 200) {
-					resolve({
-						messageId: body.message_id
-					});
-				} else {
-					reject(error);
-				}
-			});
-		});
-	}
+    incoming(req, res, cb) {
+        // Extract the body of the POST request
+        let data = req.body;
+        if(data.object === 'page') {
+            // Iterate through the page entry Array
+            data.entry.forEach(pageObj => {
+                // Iterate through the messaging Array
+                pageObj.messaging.forEach(msgEvent => {
+                    let messageObj = {
+                        sender: msgEvent.sender.id,
+                        timeOfMessage: msgEvent.timestamp,
+                        message: msgEvent.message || undefined,
+                        postback: msgEvent.postback || undefined
+                    };
 
-	// Send a text message
-	txt(id, text) {
-		let obj = {
-			recipient: {
-				id
-			},
-			message: {
-				text
-			}
-		}
+                    cb(messageObj);
+                });
+            });
+        }
+        res.send(200);
+    }
 
-		this.sendMessage(obj)
-			.catch(error => console.log(error));
-	}
+    sendMessage(payload) {
+        return new Promise((resolve, reject) => {
+            // Create an HTTP POST request
+            request({
+                uri: 'https://graph.facebook.com/v2.6/me/messages',
+                qs: {
+                    access_token: this.PAGE_ACCESS_TOKEN
+                },
+                method: 'POST',
+                json: payload
+            }, (error, response, body) => {
+                if(!error && response.statusCode === 200) {
+                    resolve({
+                        messageId: body.message_id
+                    });
+                } else {
+                    reject(error);
+                }
+            });
+        });
+    }
 
-	// Send an image message
-	img(id, url) {
-		let obj = {
-			recipient: {
-				id
-			},
-			message: {
-				attachment: {
-					type: 'image',
-					payload: {
-						url
-					}
-				}
-			}
-		}
+    // Send a text message
+    txt(id, text) {
+        let obj = {
+            recipient: {
+                id
+            },
+            message: {
+                text
+            }
+        };
 
-		this.sendMessage(obj)
-			.catch(error => console.log(error));
-	}
+        this.sendMessage(obj)
+            .catch(error => console.log(error));
+    }
+
+    // Send an image message
+    img(id, url) {
+        let obj = {
+            recipient: {
+                id
+            },
+            message: {
+                attachment: {
+                    type: 'image',
+                    payload: {
+                        url
+                    }
+                }
+            }
+        };
+
+        this.sendMessage(obj)
+            .catch(error => console.log(error));
+    }
 
 }
 
